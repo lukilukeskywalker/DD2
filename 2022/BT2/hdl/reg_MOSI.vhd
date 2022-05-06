1939735a610e60e9635fb1ccd6cab615fb7f8722
HFengCF <104440978+HFengCF@users.noreply.github.com>
2022-05-06 22:22:58 +0200

    Update reg_MOSI.vhd

--- 2022/BT2/hdl/reg_MOSI.vhd
+++ 2022/BT2/hdl/reg_MOSI.vhd
@@ -1,6 +1,6 @@
---Autor LX0809 G4 Lukitas
--- Fecha: 26-april-22
--- rev 1
+--Autor LX0809 G4 Lukitas	/ Hao
+-- Fecha: 6-may-22
+-- rev 2
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -9,13 +9,12 @@ use ieee.std_logic_unsigned.all;
 entity reg_MOSI is
 	port(clk: 	in 	std_logic;
 		nRst:	in	std_logic;
+		MOSI:	buffer std_logic;
+		tx_MOSI: in	std_logic;
 		dir: 	in	std_logic;
 		set_dato: in std_logic;
-		dato_wr: in std_logic_vector(7 downto 0);
-		tx_MOSI: in	std_logic;
-		CS: 	in std_logic;
-	     	nWR:	in std_logic;
-		MOSI:	buffer std_logic
+		nWR:	buffer std_logic;
+		dato_wr: in std_logic_vector(7 downto 0)
 	);
 end entity;
 
@@ -28,9 +27,8 @@ begin
 		if nRst = '0' then
 			tx_buffer <= (others => '0');
 		elsif clk'event and clk = '1' then
-			if set_dato = '1' and CS = '1' then --Subrutina de proteccion tras haber encontrado un bug donde si set_dato se dejaba a 1, no transmitia nada.
-												--De todas formas, no deberia poder escribirse si se esta produciendo una transmision!!!
-				if dir = '1' then
+			if set_dato = '1' then
+				if dir = '0' then
 					tx_buffer(7 downto 0) <= dato_wr;
 					nWR <= dato_wr(7);
 				else
