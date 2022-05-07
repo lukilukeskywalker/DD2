ff8f96a5a4d92cc403c088fe06e550b80bd4dd05
HFengCF <104440978+HFengCF@users.noreply.github.com>
2022-05-07 13:45:04 +0200

    Update controlador_spi.vhdl

--- 2022/BT2/hdl/controlador_spi.vhdl
+++ 2022/BT2/hdl/controlador_spi.vhdl
@@ -1,38 +1,44 @@
 -- Autor: Hao Feng
--- Fecha: 5-May-2022
--- V.2
+-- Fecha: 7-May-2022
+-- V.3
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity controlador_spi is
-port(	nRst:	in 	std_logic;
-		clk:	in	std_logic;
+port(	nRst:		in 	std_logic;
+		clk:		in	std_logic;
+		ini:		buffer 	std_logic;
+		nWR:		buffer	std_logic;
+		set_dato:	buffer	std_logic;
+		dir:		buffer	std_logic;
 
-
-		dir:	buffer	std_logic;	-- ******************* No se como incluirlo
-		nCS:	in 	std_logic;	
-
-		ini:	buffer 	std_logic;
-		nWR:	buffer	std_logic;
-		dato:	buffer std_logic_vector(7 downto 0)
+		nbusy:		in std_logic;
+		dato_wr:	buffer std_logic_vector(7 downto 0)
+		
 	);
 end entity;
 
 architecture rtl of controlador_spi is
+-- Constantes
+	-- constant fin_5ms: natural := 250000;	***********************PARA EL REAL***************************
+	constant fin_5ms: natural := 1000;		-- PARA LA SIMULACION
 --Maquina de estados
-	type state_t is (ini, set_up, leer, leyendo, espera);
+	type state_t is (inicio, set_up, leer, leyendo, espera);
 	signal estado: state_t;
 
-	signal cnt_5ms:		std_logic_vector(17 downto 0);		-- Señales para la cuentade 5 ms en el estado ini y en el estado tiempo
+	signal cnt_dir:		std_logic_vector(7 downto 0);	-- Señal para generar dir
+	signal cnt_5ms:		std_logic_vector(17 downto 0);	-- Señal para generar dir
 	signal tic_5ms:		std_logic;
 
-	signal conf_ok:		std_logic;		-- Señal que indica si la configuracion ya ha terminado
+	signal config_ok:		std_logic;		-- Señal que indica si la configuracion ya ha terminado
 	
 	signal reg_mod:		std_logic_vector(1 downto 0);
 	signal cnt_set_up:	std_logic_vector(8 downto 0);
 	signal sel_data_config:		std_logic_vector(1 downto 0);
+
+	signal ini_i:		std_logic;
 
 begin
 -- Contador de 5 milisegundos
@@ -41,7 +47,7 @@ begin
 	if nRst = '0' then
 		cnt_5ms <= (others => '0');
 	elsif clk'event and clk = '1' then
-		if estado = ini or estado = tiempo then
+		if estado = inicio or estado = espera then
 			if cnt_5ms < 250000 then
 				cnt_5ms <= cnt_5ms + 1;
 			else
@@ -54,6 +60,25 @@ begin
 end process;
 
 tic_5ms <= '1' when cnt_5ms = 250000 else '0';
+
+
+-- Contador para la lectura de datos
+process(clk,nRst)
+begin
+	if nRst = '0' then
+		cnt_dir <= (others => '0');
+	elsif clk'event and clk = '1' then
+		if estado = leyendo then
+			cnt_dir <= cnt_dir + 1;
+		else
+			cnt_dir <= (others => '0');
+		end if;
+	end if;
+end process;
+
+dir <= 	'0' when cnt_dir < 80 else
+		'0' when nWR = '1' and cnt_dir >= 80 else
+		'1';
 
 -- Contador para configurar en set_up
 
@@ -74,10 +99,10 @@ begin
 	end if;
 end process;
 
-sel_data_config <= 0 when cnt_set_up <= 80 else							-- Elige entrar en el registro 4
-				   1 when cnt_set_up <= 160 and cnt_set_up > 80 else	-- Dato de configuracion para el registro 4
-				   2 when cnt_set_up < 240 and cnt_set_up > 160 else	-- Elige entrar en el registro 1
-				   3;													-- Dato de configuracion para el registro 1
+sel_data_config <= "00" when cnt_set_up <= 80 else							-- Elige entrar en el registro 4
+				   "01" when cnt_set_up <= 160 and cnt_set_up > 80 else	-- Dato de configuracion para el registro 4
+				   "10" when cnt_set_up < 240 and cnt_set_up > 160 else	-- Elige entrar en el registro 1
+				   "11";													-- Dato de configuracion para el registro 1
 
 -- Configuracion de los registros: Necesitamos configurar REG4 y REG1
 process(clk, nRst)
@@ -87,55 +112,67 @@ begin
 	elsif clk'event and clk = '1' then
 		if estado = set_up then
 			if sel_data_config = 0 then
-				dato <= '0'&"0100010";
+				dato_wr <= '0'&"0100010";
 
 			elsif sel_data_config = 1 then
-				dato <= "00000000"			-- Configura BDU=0, BLE=0, FS=00, HR=00, ST=00, SIM=0
+				dato_wr <= "00000000";			-- Configura BDU=0, BLE=0, FS=00, HR=00, ST=00, SIM=0
 
 			elsif sel_data_config = 2 then
-				dato <= '0'&"0100000";
+				dato_wr <= '0'&"0100000";
 
 			else
-				dato <= "011100011";		-- Configura ODR=0111, LPen=0, Z=0, Y=1, X=1
+				dato_wr <= "01100011";		-- Configura ODR=0110, LPen=0, Z=0, Y=1, X=1
 			end if;
 		end if;
 	end if;
-end if;
+end process;
 
 
 config_ok <= '1' when cnt_set_up = 320 else '0';
 
-ini <= '1' when
+ini_i <= '1' when estado = set_up or estado = espera else '0';
 
-nWR <= dato(7) when estado = set_up else
+set_dato <= ini_i;
+
+nWR <= dato_wr(7) when dir = '0' and cnt_dir < 80 else nWR;
+
+process(clk,nRst)	-- Flip flop para retrasar un ciclo de reloj la señal ini
+begin
+	if nRst = '0' then		ini <= '0';
+	elsif clk'event and clk = '1' then
+		if estado = leer then
+			ini <= ini_i;
+		end if;
+	end if;
+end process;
 
 -- Maquina de estados
 process(clk,nRst)
 begin
 	if nRst = '0' then
-		estado <= ini;
+		estado <= inicio;
 	elsif clk'event and clk = '1' then
 		case estado is
-			when ini =>
+			when inicio =>
 				if tic_5ms = '1' then
 					estado <= set_up;
 				end if;
 
 			when set_up =>
-				if conf_ok = '1' then
+				if config_ok = '1' then
 					estado <= leer;
 				end if;
 
 			when leer =>
-				if nCS = '0' then
+				if nbusy = '0' then
 					estado <= leyendo;
 				end if;
 
 			when leyendo =>
-				if nCS = '1' then
-					estado <= tiempo;
+				if nbusy = '1' then
+					estado <= espera;
 				end if;
-			when tiempo =>
+			when espera =>
 				if tic_5ms = '1' then
 					estado <= leer;
 				end if;
